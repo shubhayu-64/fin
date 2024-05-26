2a17e480590d0fefc8cab48160256828a2a177ff
Shubhayu Majumdar <majumdarshubhayu@gmail.com>
2024-05-26 14:53:16 +0530

    fix: logic issue on init

--- App/preds.py
+++ App/preds.py
@@ -6,25 +6,19 @@ import logging
 
 class Predictions:
     def __init__(self, 
-                 data: pd.DataFrame, 
-                 target_colm: str,
-                 content_length: int = 512, 
+                 context_length: int = 512, 
                  horizon_length: int = 14, 
                  backend: str = "cpu", 
                  checkpoint: str = "google/timesfm-1.0-200m",
                  ) -> None:
         
         logging.info("Initializing Predictions class")
-        
-        self.data = data.copy()
-        self.target_colm = target_colm
 
-        self.default_window_size = len(self.data)/10
         self.default_step_size = horizon_length
 
         """
             Initialize timesfm model with the following parameters:
-            content_len: The max length of context on which the predictions will be done | Currently supports a max of 512
+            context_len: The max length of context on which the predictions will be done | Currently supports a max of 512
             horizon_len: The number of future days for which the predictions will be made | reccomended horizon length <= context length
             input_patch_len: Fixed value to load 200m model
             output_patch_len: Fixed value to load 200m model
@@ -33,7 +27,7 @@ class Predictions:
             backend: The backend to be used for the model | Currently supports "cpu", "gpu", "tpu"
         """
         self.tfm = timesfm.TimesFm(
-            content_len = min(content_length, 512), 
+            context_len = min(context_length, 512), 
             horizon_len=horizon_length,             
             input_patch_len=32,                     
             output_patch_len=128,                   
@@ -48,8 +42,15 @@ class Predictions:
         logging.info("Model loaded successfully")
 
     
-    def data_preprocess(self, date_colm: str) -> None:
-        self.data = self.data.astype({date_colm: 'datetime64[ns]', self.target_colm: float})
+    def data_preprocess(self, data: pd.DataFrame, target_colm: str, date_colm: str) -> None:
+        
+        self.data = data.copy()
+        self.target_colm = target_colm
+        self.default_window_size = len(self.data)//10
+        
+        self.data["ds"] = pd.to_datetime(self.data[date_colm])
+        # self.data.drop(date_colm, axis=1, inplace=True)
+        self.data = self.data.astype({self.target_colm: float})
 
     
     def _iter_split(self, current_window: int, step_size: int):
@@ -61,16 +62,6 @@ class Predictions:
         return window_data, step_size
 
 
-    def predict(self, intial_window_size: int = None, step_size: int = None, freq: str = "D"):
-        
-        logging.info("Starting predictions")
-        
-        initial_window_size = initial_window_size or self.default_window_size
-        step_size = step_size or self.default_step_size
-        
-        logging.info(f"Initial window size: {initial_window_size}")
-        logging.info(f"Step size: {step_size}")
-        
         
         # Run iterations and return a pd series of predictions
         self.data["unique_id"] = 0
